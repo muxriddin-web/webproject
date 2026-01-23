4235db238ba0003207a962151228c64d790aaf7c
muxriddin-web <otkirovmuxriddin01@gmail.com>
2026-01-23 19:52:30 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      erverSelectionTimeoutMS: 86400000   // ⏳ 10 soniyagacha kutadi
     });
     console.log(`✅ MongoDB ulandi: ${conn.connection.host}`);
   } catch (error) {
